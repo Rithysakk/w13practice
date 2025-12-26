06c736421643fd82ac209cc74e41b826cbf50798
Rithysakk <std.66122420130@ubru.ac.th>
2025-12-26 11:28:10 +0700

    Add files via upload

---
+++ lib/ui/groceries/grocery_list.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import '../../data/mock_grocery_repository.dart';
+import '../../models/grocery.dart';
+import 'grocery_form.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  int _currenTabIndex = 0; // by default
+
+  void onCreate() async {
+    // Navigate to the form screen using the Navigator push
+    Grocery? newGrocery = await Navigator.push<Grocery>(
+      context,
+      MaterialPageRoute(builder: (context) => const GroceryForm()),
+    );
+    if (newGrocery != null) {
+      setState(() {
+        dummyGroceryItems.add(newGrocery);
+      });
+    }
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: onCreate, icon: const Icon(Icons.add))],
+      ),
+
+      body: IndexedStack(
+        index: _currenTabIndex,
+        children: [
+          GroceriesTab(),
+          SeearchTab(),
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).colorScheme.primary,
+        currentIndex: _currenTabIndex,
+        onTap: (index) {
+          setState(() {
+            _currenTabIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_grocery_store),
+            label: 'Groceries',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Groceries'),
+        ],
+      ),
+    );
+  }
+}
+
+class SeearchTab extends StatefulWidget {
+  const SeearchTab({super.key});
+
+  @override
+  State<SeearchTab> createState() => _SeearchTabState();
+}
+
+class _SeearchTabState extends State<SeearchTab> {
+  String searchText = "";
+
+  void onSearchChanged(String value) {
+    setState(() {
+      searchText = value;
+    });
+  }
+
+  List<Grocery> get filteredList {
+    if (searchText.isEmpty) {
+      return dummyGroceryItems;
+    }
+    List<Grocery> result = [];
+    String lowerSearchText = searchText.toLowerCase();
+    for(Grocery g in dummyGroceryItems) {
+      if (g.name.toLowerCase().contains(lowerSearchText)) {
+        result.add(g);
+      }
+    }
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        children: [
+          TextField(onChanged: onSearchChanged),
+          SizedBox(height: 15),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredList.length,
+              itemBuilder: (context, index) => GroceryTile(
+                grocery: filteredList[index],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GroceriesTab extends StatelessWidget {
+  const GroceriesTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = const Center(child: Text('No items added yet.'));
+
+    if (dummyGroceryItems.isNotEmpty) {
+      //  Display groceries with an Item builder and  LIst Tile
+      content = ListView.builder(
+        itemCount: dummyGroceryItems.length,
+        itemBuilder: (context, index) =>
+            GroceryTile(
+          grocery: dummyGroceryItems[index],
+        ),
+      );
+    }
+    return content;
+  }
+}
+
+class GroceryTile extends StatelessWidget {
+  const GroceryTile({super.key, required this.grocery, this.onTap});
+
+  final Grocery grocery;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(width: 15, height: 15, color: grocery.category.color),
+      title: Text(grocery.name),
+      trailing: Text(grocery.quantity.toString()),
+      onTap: onTap,
+    );
+  }
+}
